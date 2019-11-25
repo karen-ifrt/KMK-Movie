75e090242a45bf9df3e6fedb70afebfe5d1104cb
karen-ifrt <55798470+karen-ifrt@users.noreply.github.com>
2019-11-25 16:56:58 +0100

    Ajout fiche film

--- Controllers/FilmController.php
+++ Controllers/FilmController.php
@@ -2,6 +2,12 @@
 
     require ('Models/Film.php');
 
+    if(isset($_GET['film'])) {
+        $id = $_GET['film'];
+    }
+
+    $movies = getMovie($id);
+
     require ('Views/FilmView.php');
 
 ?>
--- Views/FilmView.php
+++ Views/FilmView.php
@@ -4,8 +4,27 @@ include 'header.php';
 
 ?>
 
-<p>Test</p>
+<div class="container">
+    <div class="film-title">
+    <h2>
+        <?php
+        echo $movies['titre'];
+        ?>
+    </h2>
+    </div>
 
+    <div class="row">
+
+        <div class="col-md-6">
+            <img src="<?php echo 'assets/img/' . $movies['image'];  ?>">
+        </div>
+        <div class="col-md-6">
+            <p>Date de sortie : <?php echo $movies['date'] ?></p>
+            <p>Nationalité : <?php echo $movies['nationalite'] ?></p>
+            <p>Synopsis : <?php echo $movies['synopsis'] ?></p>
+        </div>
+    </div>
+</div>
 <?php
 include 'footer.php';
 ?>
